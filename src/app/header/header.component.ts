342bb512ef67858518536933f133df8ae06b51fc
SArminkaS <arminka4320@outlook.hu>
2025-04-06 15:23:13 +0200

    add header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  imports: [RouterModule],
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
